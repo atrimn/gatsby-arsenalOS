f96ca00de22e1cf977549818e3750591684f0c60
Adrian <adrianlineweaver@gmail.com>
2019-11-04 13:04:02 -0500

    check if netlify continues deployment is working

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,6 @@
 exports.handler = function(event, context, callback) {
   // your server-side functionality
-  console.log("tesst")
+  console.log("tessst")
   callback(null, {
     statusCode: 200,
     body: "Hello, World",
