cd503e0efc3025e73b49eb7eeb63580cc0c78ffe
Adrian <adrianlineweaver@gmail.com>
2019-11-04 12:55:13 -0500

    added netlify functions 5

--- functions/test.js
+++ functions/test.js
@@ -1,5 +1,6 @@
 exports.handler = async function(event, context, callback) {
   // your server-side functionality
+  console.log("test")
   callback(null, {
     statusCode: 200,
     body: "Hello, World",
