eba3d7667f4bde71a366317d38488cb25744fac2
Adrian <adrianlineweaver@gmail.com>
2019-11-04 12:58:25 -0500

    added netlify functions 6

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,4 @@
-exports.handler = async function(event, context, callback) {
+exports.handler = function(event, context, callback) {
   // your server-side functionality
   console.log("test")
   callback(null, {
